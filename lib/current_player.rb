c9ec465305f2478b82ac5b4e577e211a7348d015
Jess Jacobson <jessanova@gmail.com>
2018-10-16 20:08:02 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,12 @@
 def turn_count(board)
   turn = 0
-  board.each{|place| valid(place) ? turn+=0 :
+  board.each{|place| blank?(place) ? : turn+=1
 end
 
 def current_player
   
 end
 
-def valid?(x)
+def blank?(x)
   (x==" ") || (x.to_s.empty?) || (x.nil?) ) ? true : false
 end
